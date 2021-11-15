a9681a8f5ad21d79286d8eae45ee5e2cdb12c80d
dingzhenlin <zhenlinding@yunify.com>
2021-11-15 16:47:29 +0800

    fix:add Apache Licens and optimize code

--- pkg/middlerwareInstall.go
+++ pkg/middlerwareInstall.go
@@ -1,3 +1,16 @@
+/*
+Copyright 2020 QuanxiangCloud Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package pkg
 
 import (
@@ -16,8 +29,6 @@ func middlewareInstall(namespace,depPath,kubeconfig string,configs *Configs) (bo
 			fmt.Println("-------请输入 -k 参数获取kubeconfig信息")
 			return needMiddleware,errors.New("NO_KUBECONFIG")
 		}
-	}else {
-		kubeconfig = kubeconfig
 	}
 	if configs.Mysql.Enabled{
 		needMiddleware = true
--- pkg/statusCheck.go
+++ pkg/statusCheck.go
@@ -1,3 +1,16 @@
+/*
+Copyright 2020 QuanxiangCloud Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package pkg
 
 import (
@@ -20,8 +33,6 @@ func statusCheck(kubeconfig, namespace string) error {
 			fmt.Println("-------请输入 -k 参数获取kubeconfig信息")
 			return errors.New("NO_KUBECONFIG")
 		}
-	}else {
-		kubeconfig = kubeconfig
 	}
 	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
 	if err != nil {
