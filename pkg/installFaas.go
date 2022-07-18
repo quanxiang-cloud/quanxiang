ff2b8fb7fbc9ebdfc7811db8d24901185e9dae38
dingzhenlin <zhenlinding@yunify.com>
2022-07-18 16:51:20 +0800

    uppdate: include new faas and some bug fix

---
+++ pkg/installFaas.go
@@ -0,0 +1,132 @@
+package pkg
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"path/filepath"
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+type Gits struct {
+	ID         string `gorm:"column:id;type:varchar(64);PRIMARY_KEY" json:"id"`
+	Host       string `gorm:"column:host;type:varchar(200);" json:"host"`
+	KnownHosts string `gorm:"column:known_hosts;type:text;" json:"knownHosts"`
+	SSH        string `gorm:"column:ssh;type:text;" json:"ssh"`
+	Token      string `gorm:"column:token;type:text;" json:"token"`
+	Name       string `gorm:"column:name;type:varchar(200);" json:"name"`
+
+	CreatedAt int64  `gorm:"column:created_at;type:bigint; " json:"createdAt,omitempty" `
+	UpdatedAt int64  `gorm:"column:updated_at;type:bigint; " json:"updatedAt,omitempty" `
+	DeletedAt int64  `gorm:"column:deleted_at;type:bigint; " json:"deletedAt,omitempty" `
+	CreatedBy string `gorm:"column:created_by;type:varchar(64); " json:"createdBy,omitempty"` //创建者
+	UpdatedBy string `gorm:"column:updated_by;type:varchar(64); " json:"updatedBy,omitempty"` //创建者
+	DeletedBy string `gorm:"column:deleted_by;type:varchar(64); " json:"deletedBy,omitempty"` //删除者
+	TenantID  string `gorm:"column:tenant_id;type:varchar(64); " json:"tenantID"`             //租户id
+}
+type Dockers struct {
+	ID        string `gorm:"column:id;type:varchar(64);PRIMARY_KEY" json:"id"`
+	Host      string `gorm:"column:host;type:varchar(200);" json:"host"`
+	UserName  string `gorm:"column:user_name;type:varchar(64);" json:"userName"`
+	NameSpace string `gorm:"column:name_space;type:varchar(64);" json:"nameSpace"`
+	Secret    string `gorm:"column:secret;type:text;" json:"secret"`
+	Name      string `gorm:"column:name;type:varchar(64);" json:"name"`
+
+	CreatedAt int64  `gorm:"column:created_at;type:bigint; " json:"createdAt,omitempty" `
+	UpdatedAt int64  `gorm:"column:updated_at;type:bigint; " json:"updatedAt,omitempty" `
+	DeletedAt int64  `gorm:"column:deleted_at;type:bigint; " json:"deletedAt,omitempty" `
+	CreatedBy string `gorm:"column:created_by;type:varchar(64); " json:"createdBy,omitempty"` //创建者
+	UpdatedBy string `gorm:"column:updated_by;type:varchar(64); " json:"updatedBy,omitempty"` //创建者
+	DeletedBy string `gorm:"column:deleted_by;type:varchar(64); " json:"deletedBy,omitempty"` //删除者
+	TenantID  string `gorm:"column:tenant_id;type:varchar(64); " json:"tenantID"`             //租户id
+}
+
+func applyGitSeret(host, know_host, ssh,kubeconfig, namespace string) error {
+	if kubeconfig == "" || kubeconfig == "~/.kube/config" {
+		if home := homedir.HomeDir(); home != "" {
+			kubeconfig = filepath.Join(home, ".kube", "config")
+		} else {
+			fmt.Println("-------请输入 -k 参数获取kubeconfig信息")
+			return errors.New("NO_KUBECONFIG")
+		}
+	}
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	secret := clientset.CoreV1().Secrets(namespace)
+	data := make(map[string][]byte)
+	// ssh-keyscan github.com | base64
+	data["known_hosts"] = []byte(know_host)
+	data["ssh-privatekey"] = []byte(ssh)
+
+	tekton := make(map[string]string)
+	tekton["tekton.dev/git-0"] = host
+
+	s := &v1.Secret{
+		Type: v1.SecretTypeSSHAuth,
+		ObjectMeta: ctrl.ObjectMeta{
+			Name:        "rsa",
+			Namespace:   namespace,
+			Annotations: tekton,
+		},
+		Data: data,
+	}
+	options := metav1.CreateOptions{}
+	_, err = secret.Create(context.Background(), s, options)
+	if err != nil {
+		return err
+	}
+	return err
+}
+
+func applyHarbor(username,password,server string) error {
+	command := "kubectl create secret docker-registry faas-harbor --docker-username="+username+" --docker-password="+password+" --docker-server="+server+" -n builder"
+	err := execBash(command)
+	if err != nil {
+		return err
+	}
+	command = "kubectl create secret docker-registry faas-harbor --docker-username="+username+" --docker-password="+password+" --docker-server="+server+" -n serving"
+	err = execBash(command)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func applyFaasSql(msserver,msport,username,password,token,git_host,knows_hosts,ssh,docker_host,docker_username,docker_pass string) error {
+	gits := Gits{}
+	gits.ID="J2P57lAS"
+	gits.Host = git_host
+	gits.Token = token
+	gits.KnownHosts = knows_hosts
+	gits.Name = "rsa"
+	gits.SSH = ssh
+	fqn := username+ ":" + password+"@tcp("+msserver+":"+msport+")/faas?charset=utf8&parseTime=True&loc=Local"
+	db, err := gorm.Open("mysql", fqn)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+	db.Create(&gits)
+	docker := Dockers{}
+	docker.ID = "1"
+	docker.Host = docker_host
+	docker.Name = "faas-harbor"
+	docker.UserName = docker_username
+	docker.Secret = docker_pass
+	db.Create(&docker)
+	return nil
+}
--- pkg/modifyValues.go
+++ pkg/modifyValues.go
@@ -16,81 +16,102 @@ package pkg
 import (
 	"bytes"
 	"fmt"
-	"github.com/satori/go.uuid"
-	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"strings"
 	"text/template"
+
+	uuid "github.com/satori/go.uuid"
+	"gopkg.in/yaml.v2"
 )
+
 //project
 type values struct {
-	Image struct{
+	Image struct {
 		Name string `yaml:"name"`
 		Repo string `yaml:"repo"`
-		Tag string `yaml:"tag"`
+		Tag  string `yaml:"tag"`
 	}
-	Namespace string `yaml:"namespace"`
-	Domain string `yaml:"domain"`
+	Namespace  string `yaml:"namespace"`
+	Domain     string `yaml:"domain"`
 	Mongo_host string `yaml:"mongo_host"`
-	Args struct{
-		Enabled bool `yaml:"enabled"`
+	Args       struct {
+		Enabled  bool   `yaml:"enabled"`
 		Endpoint string `yaml:"endpoint"`
-		Ip string `yaml:"ip"`
+		Ip       string `yaml:"ip"`
 	}
-	Kafka struct{
+	Kafka struct {
 		Value string `yaml:"value"`
 	}
-	Redis struct{
-		Host string `yaml:"host"`
+	Redis struct {
+		Host     string `yaml:"host"`
 		Password string `yaml:"password"`
 	}
 	ImagePullSecrets string `yaml:"imagePullSecrets"`
-	Service struct{
-		Type string `yaml:"type"`
-		Port int `yaml:"port"`
-		RpcPort int `yaml:"rpcPort"`
+	Service          struct {
+		Type    string `yaml:"type"`
+		Port    int    `yaml:"port"`
+		RpcPort int    `yaml:"rpcPort"`
 	}
-	NameOverride string `yaml:"nameOverride"`
-	FullnameOverride string `yaml:"fullnameOverride"`
+	NameOverride       string `yaml:"nameOverride"`
+	FullnameOverride   string `yaml:"fullnameOverride"`
 	Websocket_hostname string `yaml:"websocket_hostname"`
-	Home_hostname string `yaml:"home_hostname"`
-	Portal_hostname string `yaml:"portal_hostname"`
-	ServiceAccount struct{
-		Name string `yaml:"name"`
-	}`yaml:"serviceAccount"`
+	Home_hostname      string `yaml:"home_hostname"`
+	Portal_hostname    string `yaml:"portal_hostname"`
+	Vendor             Vendor `yaml:"vendor"`
+	ServiceAccount     struct {
+		Create bool   `yaml:create`
+		Name   string `yaml:"name"`
+	} `yaml:"serviceAccount"`
+	ClusterRole ClusterRole `json:"clusterRole"`
 	PodSecurityContext map[string]interface{} `yaml:"podSecurityContext"`
-	SecurityContext map[string]interface{} `yaml:"securityContext"`
-	Config struct{
-		JwtKey string `yaml:"jwtKey"`
-		Mysql Mysql
-		Redis Redis
+	SecurityContext    map[string]interface{} `yaml:"securityContext"`
+	Config             struct {
+		JwtKey  string `yaml:"jwtKey"`
+		Mysql   Mysql
+		Redis   Redis
 		Elastic Elastic
-		Kafka Kafka
-		Mongo Mongo
-		Etcd Etcd
+		Kafka   Kafka
+		Mongo   Mongo
+		Etcd    Etcd
 		Storage Storage
-		Email Email
+		Email   Email
 	}
-	Ingress struct{
+	Ingress struct {
 		Enabled bool `yaml:"enabled"`
-		Hosts []struct{
-			Host string  `yaml:"host"`
+		Hosts   []struct {
+			Host string `yaml:"host"`
 			//Paths []struct{
 			//	Path string `yaml: path`
 			//	FullName string `yaml: fullName`
 			//	SvcPort int `yaml svcPort`
 			//}`yaml: paths`
-			Paths []map[string]interface{}`yaml:"paths"`
-		}`yaml:"hosts"`
+			Paths []map[string]interface{} `yaml:"paths"`
+		} `yaml:"hosts"`
 	}
-	App struct{
-		Kubernetes struct{
-			Io struct{
+	App struct {
+		Kubernetes struct {
+			Io struct {
 				Name string `yaml:"name"`
-			}`yaml:"io"`
-		}`yaml:"kubernetes"`
+			} `yaml:"io"`
+		} `yaml:"kubernetes"`
 	}
 }
+
+type Vendor struct {
+	Protocol string `yaml:"protocol"`
+	Hostname string `yaml:"hostname"`
+	Port     int    `yaml:"port"`
+}
+
+type Annotations struct {
+	RbacAuthorizationKubernetesIoAutoupdate string `json:"rbac.authorization.kubernetes.io/autoupdate"`
+}
+type ClusterRole struct {
+	Create bool `json:"create"`
+	Name string `json:"name"`
+	Annotations Annotations `json:"annotations"`
+}
+
 //func ReadAllValuesFile(configFile string) (*values,error) {
 //	all := new(values)
 //	data,err := ioutil.ReadFile(configFile)
@@ -104,134 +125,143 @@ type values struct {
 //	}
 //	return all,nil
 //}
-func ModifyValuesFile(filepath,namespace string,configs *Configs,ngGateWay bool) error {
+func ModifyValuesFile(filepath, namespace string, configs *Configs, ngGateWay bool) error {
 	//all,err := ReadAllValuesFile(configFile)
 	//if err != nil {
 	//	return err
 	//}
 	value := new(values)
-	data,err := ioutil.ReadFile(filepath)
+	data, err := ioutil.ReadFile(filepath)
 	if err != nil {
 		fmt.Println("读取value文件失败")
 		return err
 	}
-	err = yaml.Unmarshal(data,value)
+	err = yaml.Unmarshal(data, value)
 	if err != nil {
 		fmt.Println("读取数据失败")
 		return err
 	}
-	if configs.Mysql.Enabled{
-		value.Config.Mysql.Host,err = AddrParase(configs.Config.Mysql.Host,namespace)
+	if configs.Mysql.Enabled {
+		value.Config.Mysql.Host, err = AddrParase(configs.Config.Mysql.Host, namespace)
 		if err != nil {
 			return err
 		}
 		value.Config.Mysql.Password = configs.Config.Mysql.Password
 		value.Config.Mysql.User = configs.Config.Mysql.User
 		value.Config.Mysql.Log = configs.Config.Mysql.Log
-	}else {
+	} else {
 		value.Config.Mysql.Host = configs.Config.Mysql.Host
 		value.Config.Mysql.Password = configs.Config.Mysql.Password
 		value.Config.Mysql.User = configs.Config.Mysql.User
 		value.Config.Mysql.Log = configs.Config.Mysql.Log
 	}
 	//value.Config.Redis = configs.Config.Redis
-	if strings.Contains(filepath,"flow"){
+	if strings.Contains(filepath, "flow") {
 		value.Redis.Host = ""
-		for _,v := range value.Config.Redis.Addrs{
+		for _, v := range value.Config.Redis.Addrs {
 			value.Redis.Host += v
 			value.Redis.Host += ","
 		}
-		value.Redis.Host = strings.TrimSuffix(value.Redis.Host,",")
+		value.Redis.Host = strings.TrimSuffix(value.Redis.Host, ",")
 		value.Redis.Password = configs.Config.Redis.Password
 	}
-	if configs.Redis.Enabled{
-		for i,_ := range value.Config.Redis.Addrs{
-			value.Config.Redis.Addrs[i],err = AddrParase(configs.Config.Redis.Addrs[i],namespace)
+	if configs.Redis.Enabled {
+		for i, _ := range value.Config.Redis.Addrs {
+			value.Config.Redis.Addrs[i], err = AddrParase(configs.Config.Redis.Addrs[i], namespace)
 			if err != nil {
 				return err
 			}
 		}
 		value.Config.Redis.Username = configs.Config.Redis.Username
 		value.Config.Redis.Password = configs.Config.Redis.Password
-	}else {
+	} else {
 		value.Config.Redis = configs.Config.Redis
 	}
-	if configs.Elastic.Enabled{
-		ev,_ := AddrParase(configs.Config.Elastic.Host[0],namespace)
+	if configs.Elastic.Enabled {
+		ev, _ := AddrParase(configs.Config.Elastic.Host[0], namespace)
 		elas_v := []string{ev}
 		value.Config.Elastic.Host = elas_v
 		value.Config.Elastic.Log = configs.Config.Elastic.Log
-	}else{
+	} else {
 		value.Config.Elastic = configs.Config.Elastic
 	}
-	if configs.Mongo.Enabled{
-		value.Config.Mongo.Hosts[0],err = AddrParase(configs.Config.Mongo.Hosts[0],namespace)
+	if configs.Mongo.Enabled {
+		value.Config.Mongo.Hosts[0], err = AddrParase(configs.Config.Mongo.Hosts[0], namespace)
 		value.Config.Mongo.Credential = configs.Config.Mongo.Credential
 		value.Config.Mongo.Direct = configs.Config.Mongo.Direct
-	}else {
+	} else {
 		value.Config.Mongo = configs.Config.Mongo
 	}
-	if configs.Etcd.Enabled{
-		value.Config.Etcd.Addrs[0],err = AddrParase(configs.Config.Etcd.Addrs[0],namespace)
-		for i,_ := range value.Config.Etcd.Addrs{
-			if i == 0{
+	if configs.Etcd.Enabled {
+		value.Config.Etcd.Addrs[0], err = AddrParase(configs.Config.Etcd.Addrs[0], namespace)
+		for i, _ := range value.Config.Etcd.Addrs {
+			if i == 0 {
 				continue
-			}else{
+			} else {
 				value.Config.Etcd.Addrs[i] = ""
 			}
 		}
 		value.Config.Etcd.Username = configs.Config.Etcd.Username
 		value.Config.Etcd.Password = configs.Config.Etcd.Password
-	}else {
+	} else {
 		value.Config.Etcd.Addrs = configs.Config.Etcd.Addrs
 		value.Config.Etcd.Username = configs.Config.Etcd.Username
 		value.Config.Etcd.Password = configs.Config.Etcd.Password
 	}
-	if strings.Contains(filepath,"portal"){
-		value.Ingress.Hosts[0].Host = "portal." +  configs.Domain
+	if strings.Contains(filepath, "portal") {
+		value.Ingress.Hosts[0].Host = "portal." + configs.Domain
 		value.Websocket_hostname = "ws." + configs.Domain
 		value.Home_hostname = "home." + configs.Domain
 		value.Portal_hostname = "portal." + configs.Domain
+		value.Vendor.Hostname = "vendors." + configs.Domain
+		value.Vendor.Port = 80
+		value.Vendor.Protocol = "http"
 	}
-	if strings.Contains(filepath,"home"){
-		value.Ingress.Hosts[0].Host = "home." +  configs.Domain
+	if strings.Contains(filepath, "home") {
+		value.Ingress.Hosts[0].Host = "home." + configs.Domain
 		value.Websocket_hostname = "ws." + configs.Domain
 		value.Home_hostname = "home." + configs.Domain
 		value.Portal_hostname = "portal." + configs.Domain
+		value.Vendor.Hostname = "vendors." + configs.Domain
+		value.Vendor.Port = 80
+		value.Vendor.Protocol = "http"
 	}
-	if strings.Contains(filepath,"fileserver"){
-		value.Ingress.Hosts[0].Host = "*.fs." +  configs.Domain
+	if strings.Contains(filepath, "vendors") {
+		value.Ingress.Hosts[0].Host = "vendors." + configs.Domain
+	}
+	if strings.Contains(filepath, "fileserver") {
+		value.Ingress.Hosts[0].Host = "*.fs." + configs.Domain
 		value.Domain = configs.Domain
 	}
-	if strings.Contains(filepath,"polygate"){
-		value.Ingress.Hosts[0].Host = "ws." +  configs.Domain
-		value.Ingress.Hosts[1].Host = "api." +  configs.Domain
+	if strings.Contains(filepath, "polygate") {
+		value.Ingress.Hosts[0].Host = "ws." + configs.Domain
+		value.Ingress.Hosts[1].Host = "api." + configs.Domain
 	}
-	if strings.Contains(filepath,"polyapi"){
-		value.Ingress.Hosts[0].Host = "polyapi." +  configs.Domain
+	if strings.Contains(filepath, "polyapi") {
+		value.Ingress.Hosts[0].Host = "polyapi." + configs.Domain
 	}
-	if strings.Contains(filepath,"form"){
+	if strings.Contains(filepath, "form") {
 		value.Mongo_host = value.Config.Mongo.Hosts[0]
 	}
-	if configs.Kafka.Enabled{
-		value.Config.Kafka.Broker[0],err = AddrParase(configs.Config.Kafka.Broker[0],namespace)
-		for i,_ := range value.Config.Kafka.Broker{
-			if i == 0{
+	if configs.Kafka.Enabled {
+		value.Config.Kafka.Broker[0], err = AddrParase(configs.Config.Kafka.Broker[0], namespace)
+		for i, _ := range value.Config.Kafka.Broker {
+			if i == 0 {
 				continue
-			}else{
+			} else {
 				value.Config.Kafka.Broker[i] = ""
 			}
 		}
 		value.Kafka.Value = value.Config.Kafka.Broker[0]
-	}else {
+	} else {
 		value.Config.Kafka = configs.Config.Kafka
 		value.Kafka.Value = ""
-		for _,k := range configs.Config.Kafka.Broker{
-			k_n,_ := AddrParase(k,namespace)
+		for _, k := range configs.Config.Kafka.Broker {
+			k_n, _ := AddrParase(k, namespace)
 			value.Kafka.Value += k_n
 			value.Kafka.Value += ","
 		}
-		value.Kafka.Value = strings.TrimSuffix(value.Kafka.Value,",")
+		value.Kafka.Value = strings.TrimSuffix(value.Kafka.Value, ",")
 	}
 
 	value.Image.Repo = configs.Image.Repo
@@ -243,26 +273,26 @@ func ModifyValuesFile(filepath,namespace string,configs *Configs,ngGateWay bool)
 	value.Args.Enabled = configs.Args.Enabled
 	value.Args.Ip = configs.Args.Ip
 	value.Domain = configs.Domain
-	if strings.Contains(filepath,"warden"){
+	if strings.Contains(filepath, "warden") {
 		value.Config.JwtKey = uuid.NewV4().String()
 	}
 
-	if ngGateWay{
+	if ngGateWay {
 		value.Websocket_hostname = value.Websocket_hostname + ":32032"
 		value.Portal_hostname = value.Portal_hostname + ":32032"
 		value.Home_hostname = value.Home_hostname + ":32032"
-		for _,m := range value.Config.Storage.Storages{
-			if m.Name == "minio"{
+		for _, m := range value.Config.Storage.Storages {
+			if m.Name == "minio" {
 				m.Domain = m.Domain + ":32032"
 			}
 		}
 	}
-	data_file,err := yaml.Marshal(value)
+	data_file, err := yaml.Marshal(value)
 	if err != nil {
 		fmt.Println("修改后解析错误")
 		return err
 	}
-	err = ioutil.WriteFile(filepath,data_file,0644)
+	err = ioutil.WriteFile(filepath, data_file, 0644)
 	if err != nil {
 		fmt.Println("写出到文件错误")
 		return err
@@ -271,15 +301,15 @@ func ModifyValuesFile(filepath,namespace string,configs *Configs,ngGateWay bool)
 	return nil
 }
 
-func AddrParase(addr,namespace string) (string,error) {
+func AddrParase(addr, namespace string) (string, error) {
 	tmpl, err := template.New("test").Parse(addr)
 	if err != nil {
-		return "",err
+		return "", err
 	}
 	var buf bytes.Buffer
-	err = tmpl.Execute(&buf,namespace)
+	err = tmpl.Execute(&buf, namespace)
 	if err != nil {
-		return "",err
+		return "", err
 	}
-	return buf.String(),nil
+	return buf.String(), nil
 }
