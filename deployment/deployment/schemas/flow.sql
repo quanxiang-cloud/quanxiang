6510c5ad9f20ba5936543c74838bfc8e9d939874
Kevin Li <kevinlee6780@gmail.com>
2022-09-19 14:09:41 +0800

    fix: add request_id into flow

--- deployment/deployment/schemas/flow.sql
+++ deployment/deployment/schemas/flow.sql
@@ -315,3 +315,5 @@ alter table flow_variables
 
 alter table flow_instance_variables
     modify code varchar(200) default '' not null comment '变量标识';
+
+alter table flow_instance add request_id varchar(200) null;
